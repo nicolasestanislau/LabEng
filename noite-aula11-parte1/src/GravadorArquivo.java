949caa9df49e4061b29df71d33ab75c8da17f1cb
nicolasestanislau <nicolas_estanislau@hotmail.com>
2021-05-05 22:53:03 -0300

    Socket com Java

---
+++ noite-aula11-parte1/src/GravadorArquivo.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class GravadorArquivo {
+
+    public static void main(String[] args) {
+
+        File f = new File("C:/temp/teste.txt");
+
+        try {
+            FileWriter fw = new FileWriter( f );
+
+            fw.write("Antonio");
+
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Pode Gravar ==> " + f.canWrite());
+    }
+}
